7d597cc92df9375f6b1429ddff3d3d7db2957641
rokoga <rokoga@gmail.com>
2021-01-18 15:55:28 -0300

    Change default uri and storeId param

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,73 +3,102 @@ import './styles.css';
 import CancelIcon from '@material-ui/icons/Cancel';
 import axios from 'axios';
 
-export default function Home() {
-    const [name, setName] = useState("");
-    const [phone, setPhone] = useState("");
-    const [phoneList, setPhoneList] = useState([]);
+export default function Home({ storeId = '6005d6adf10778d52e887d89' }) {
+  const API_URI = 'http://localhost:8080';
 
-    useEffect(function () {
-        //Remover e add no backend
-        axios.get('http://192.168.15.5:8080/consumers/6000a6b2863429579782d605')
-            .then(function (response) {
-                // handle success
-                console.log(response);
-                //iterar no array response.data
-                //setPhoneList(response.data.map((element) => element.phone))
-                setPhoneList(response.data)
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-    }, [])
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [phoneList, setPhoneList] = useState([]);
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        axios.put('http://192.168.15.5:8080/consumer', { "storeid": "6000a6b2863429579782d605", "name": name, "phone": phone })
-            .then(function () {
-                setPhoneList([...phoneList, { "name": name, "phone": phone }]);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    }
+  useEffect(function () {
+    //Remover e add no backend
+    axios
+      .get(`${API_URI}/consumers/${storeId}`)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        //iterar no array response.data
+        //setPhoneList(response.data.map((element) => element.phone))
+        if (response.data) setPhoneList(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
 
-    function handleChangePhone(e) {
-        setPhone(e.target.value);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .put(`${API_URI}/consumer`, {
+        storeid: storeId,
+        name: name,
+        phone: phone,
+      })
+      .then(function () {
+        setPhoneList([...phoneList, { name: name, phone: phone }]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-    function handleChangeName(e) {
-        setName(e.target.value);
-    }
+  function handleChangePhone(e) {
+    setPhone(e.target.value);
+  }
 
-    const checkPhone = (phone) => (phoneList.some(elem => elem.phone === phone)) ?
-        <button type="submit">Adicionar</button> :
-        // <h4 style={{ color: 'red' }}>O número {phone} já está na fila.</h4> :
-        <button type="submit">Adicionar</button>
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
 
-    function handleRemove(index, phone) {
-        //console.log(index);
-        axios.delete(`http://192.168.15.5:8080/consumer/6000a6b2863429579782d605/${phone}`)
-            .then(function () {
-                const newArray = [...phoneList];
-                newArray.splice(index, 1);
-                setPhoneList(newArray);
-            })
-            .catch(function (error) { console.log(error) });
-    }
+  const checkPhone = (phone) =>
+    phoneList.some((elem) => elem.phone === phone) ? (
+      <button type='submit'>Adicionar</button>
+    ) : (
+      // <h4 style={{ color: 'red' }}>O número {phone} já está na fila.</h4> :
+      <button type='submit'>Adicionar</button>
+    );
 
-    return (
-        <div className="queue-wrapper">
-            <ul className="queue-list">
-                {phoneList.map((element, index) => <li key={element.phone}>{element.phone} - {element.name} <CancelIcon onClick={() => handleRemove(index, element.phone)}>{index}</CancelIcon></li>)}
-            </ul>
-            <form className="form-queue" onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChangeName} placeholder="Nome"></input>
-                <input type="text" onChange={handleChangePhone} placeholder="Telefone"></input>
-                {checkPhone(phone)}
-                {/* <button type="submit">Adicionar</button> */}
-            </form>
-        </div>
-    )
+  function handleRemove(index, phone) {
+    //console.log(index);
+    axios
+      .delete(`${API_URI}/consumer/${storeId}/${phone}`)
+      .then(function () {
+        const newArray = [...phoneList];
+        newArray.splice(index, 1);
+        setPhoneList(newArray);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className='queue-wrapper'>
+      <ul className='queue-list'>
+        {phoneList.map((element, index) => (
+          <li key={element.phone}>
+            {element.phone} - {element.name}{' '}
+            <CancelIcon onClick={() => handleRemove(index, element.phone)}>
+              {index}
+            </CancelIcon>
+          </li>
+        ))}
+      </ul>
+      <form className='form-queue' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          onChange={handleChangeName}
+          placeholder='Nome'
+        ></input>
+        <input
+          type='text'
+          onChange={handleChangePhone}
+          placeholder='Telefone'
+        ></input>
+        {checkPhone(phone)}
+        {/* <button type="submit">Adicionar</button> */}
+      </form>
+    </div>
+  );
 }
